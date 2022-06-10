17337b2cf7c78e6c764f37df842cbf2565939ebd
serpro <none>
2022-06-10 18:53:13 +0300

    nc.w.match generic enum

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::fmt;
 use std::collections::HashMap;
 
 struct Home {
-  name: &'static str,
-  rooms: HashMap<&'static str, Room>,
+    name: &'static str,
+    rooms: HashMap<&'static str, Room>,
 }
 
+#[derive(Debug)]
 struct Room {
-  name: &'static str,
-  devices: HashMap<&'static str, SmartHomeUnitType>, 
+    name: &'static str,
+    devices: HashMap<&'static str, SmartHomeUnitType>,
 }
-
 
 #[derive(Debug)]
 struct Socket {
@@ -42,61 +43,56 @@ trait SmartHomeUnit {
     fn is_on(&self) -> &'static str;
 }
 
-
 impl Home {
-    fn new (name: &'static str) -> Home {
+    fn new(name: &'static str) -> Home {
         Home {
             name,
             rooms: HashMap::new(),
         }
     }
-    
+
     fn add_room(&mut self, name: &'static str) {
         self.rooms.insert(name, Room::new(name));
     }
 
     fn get_rooms_list(&self) {
-        for (_key, val) in self.rooms.iter()  {
+        for (_key, val) in self.rooms.iter() {
             println!("{}", val.name);
         }
     }
 }
 
 impl Room {
-    fn new (name: &'static str) -> Room {
-        Room { 
-            name, 
+    fn new(name: &'static str) -> Room {
+        Room {
+            name,
             devices: HashMap::new(),
         }
     }
-    
+
     fn add_device_socket(&mut self, name: &'static str) {
-        self.devices.insert(name,SmartHomeUnitType::Socket(
-                SmartHomeUnit::new(name)
-            )
-        );
+        self.devices
+            .insert(name, SmartHomeUnitType::Socket(SmartHomeUnit::new(name)));
     }
 
     fn add_device_thermometer(&mut self, name: &'static str) {
         self.devices.insert(
-            name, 
-            SmartHomeUnitType::Thermometer(
-                SmartHomeUnit::new(name)
-            )
+            name,
+            SmartHomeUnitType::Thermometer(SmartHomeUnit::new(name)),
         );
     }
-    
+
     fn get_devices_list(&self) {
         // dbg!(self.devices)
         for (_key, device) in self.devices.iter() {
             match &device {
+                SmartHomeUnitType::Socket(Socket { name, .. }) => {
+                    println!("{:?}", name)
+                }
 
-                SmartHomeUnitType::Socket( Socket { name, .. }
-                ) => { println!("{:?}", name) },
-
-                SmartHomeUnitType::Thermometer( Thermometer { name, .. }
-                ) => { println!("{:?}", name) },
-            
+                SmartHomeUnitType::Thermometer(Thermometer { name, .. }) => {
+                    println!("{:?}", name)
+                }
             }
         }
     }
@@ -188,42 +184,50 @@ impl SmartHomeUnit for Thermometer {
     }
 }
 
+// fn get_report(device: &SmartHomeUnitType) {
+fn get_report<T: fmt::Debug>(device: T) {
+    println!("\nREPORT: \n");
+		println!("{:?}", device);
 
-fn get_report(device: &SmartHomeUnitType) {
-	println!("\nREPORT: \n");
+    // match &device { 
+		// 	Some(Socket { 
+		// 		name, 
+		// 		about, 
+		// 		on_status, 
+		// 		current_power_consumption 
+		// 	}) => {
+    //         println!(
+    //             "Name: {}\nAbout: {}\nIs on: {}\nConsumption: {}",
+    //             name, about, on_status, current_power_consumption,
+    //         )
+		// 	}
+		// }
 
     match &device {
-				//name: "Socket2", 
-				// about: "about Socket", 
-				// on_status: false, 
-				// current_power_consumption: 0
+			
+        SmartHomeUnitType::Socket(Socket {
+            name,
+            about,
+            on_status,
+            current_power_consumption,
+        }) => {
+            println!(
+                "Name: {}\nAbout: {}\nIs on: {}\nConsumption: {}",
+                name, about, on_status, current_power_consumption,
+            )
+        }
 
-        SmartHomeUnitType::Socket( Socket { 
-					name, 
-					about, 
-					on_status,
-					current_power_consumption,
-				}
-        ) => { println!("Name: {}\nAbout: {}\nIs on: {}\nConsumption: {}", 
-					name, 
-					about, 
-					on_status, 
-					current_power_consumption,
-				) },
-
-        SmartHomeUnitType::Thermometer( Thermometer {
-					name, 
-					about, 
-					on_status, 
-					current_temperature,
-				}
-        ) => { println!("Name: {}\nAbout: {}\nIs on: {}\nTemperature: {}", 
-					name,
-					about,
-					on_status,
-					current_temperature,
-				) },
-
+        SmartHomeUnitType::Thermometer(Thermometer {
+            name,
+            about,
+            on_status,
+            current_temperature,
+        }) => {
+            println!(
+                "Name: {}\nAbout: {}\nIs on: {}\nTemperature: {}",
+                name, about, on_status, current_temperature,
+            )
+        }
     }
 }
 
@@ -231,7 +235,7 @@ fn get_report(device: &SmartHomeUnitType) {
 
 fn main() {
     // Библиотека предоставляет структуру дома в комнатах которого расположены устройства.
-    // - Дом имеет название и содержит несколько помещений. 
+    // - Дом имеет название и содержит несколько помещений.
     println!("Дом имеет название и содержит несколько помещений.");
     let mut home_1: Home = Home::new("Home1");
 
@@ -243,46 +247,59 @@ fn main() {
     // - Библиотека позволяет запросить список помещений в доме.
     println!("\nСписок помещений: ");
     home_1.get_rooms_list();
-    
-    // - Помещение имеет уникальное название 
+
+    // - Помещение имеет уникальное название
     println!("\nУникальное название помещения: ");
     println!("{}", home_1.rooms["bedroom1"].name);
-    
+
     //и содержит названия нескольких устройств.
     println!("\nНазвания нескольких устройств: ");
-    home_1.rooms.get_mut("kitchen1").unwrap().add_device_socket("Socket2");
-    home_1.rooms.get_mut("kitchen1").unwrap().add_device_socket("Socket3");
-    
-    home_1.rooms.get_mut("kitchen1").unwrap().add_device_thermometer("Thermometer2");
-    home_1.rooms.get_mut("kitchen1").unwrap().add_device_thermometer("Thermometer3");
+    home_1
+        .rooms
+        .get_mut("kitchen1")
+        .unwrap()
+        .add_device_socket("Socket2");
+    home_1
+        .rooms
+        .get_mut("kitchen1")
+        .unwrap()
+        .add_device_socket("Socket3");
+
+    home_1
+        .rooms
+        .get_mut("kitchen1")
+        .unwrap()
+        .add_device_thermometer("Thermometer2");
+    home_1
+        .rooms
+        .get_mut("kitchen1")
+        .unwrap()
+        .add_device_thermometer("Thermometer3");
 
     // - Устройство имеет уникальное в рамках помещения имя.
     println!("\nУникальное имя устройства:");
     // println!("{}", home_1.rooms["kitchen1"].devices["Socket2"].);
-    
+
     // - Библиотека позволяет получать список устройств в помещении.
     println!("\nСписок устройств в помещении");
     home_1.rooms["kitchen1"].get_devices_list();
-    
-    // - Библиотека имеет функцию, возвращающую текстовый отчёт о состоянии дома. 
-    //     Эта функция принимает в качестве аргумента обобщённый тип, позволяющий 
-    //     получить текстовую информацию о состоянии устройства, для включения в отчёт. 
-    //     Эта информация должна предоставляться для каждого устройства на основе 
-    //     данных о положении устройства в доме: имени комнаты и имени устройства. 
-    //     Если устройство не найдено в источнике информации, то вместо текста о 
+
+    // - Библиотека имеет функцию, возвращающую текстовый отчёт о состоянии дома.
+    //     Эта функция принимает в качестве аргумента обобщённый тип, позволяющий
+    //     получить текстовую информацию о состоянии устройства, для включения в отчёт.
+    //     Эта информация должна предоставляться для каждого устройства на основе
+    //     данных о положении устройства в доме: имени комнаты и имени устройства.
+    //     Если устройство не найдено в источнике информации, то вместо текста о
     //     состоянии вернуть сообщение об ошибке.
-    
 
     get_report(&home_1.rooms["kitchen1"].devices["Socket2"]);
-		get_report(&home_1.rooms["kitchen1"].devices["Socket3"]);
-		get_report(&home_1.rooms["kitchen1"].devices["Thermometer3"])
+    get_report(&home_1.rooms["kitchen1"].devices["Socket3"]);
+    get_report(&home_1.rooms["kitchen1"].devices["Thermometer3"]);
+    // get_report(&home_1.rooms["kitchen1"]);
 
-    // - Привести пример типа, предоставляющего текстовую информацию об устройствах 
-    //     в доме для составления отчёта. Шаблон для описания сущностей библиотеки: 
+    // - Привести пример типа, предоставляющего текстовую информацию об устройствах
+    //     в доме для составления отчёта. Шаблон для описания сущностей библиотеки:
     //     https://gist.github.com/76dff7177f19ff7e802b1e121865afe4
-
-    
-
 
     // let test = Home1.rooms["bedroom1"].name;
     // println!("{}", test);
