b245afd04ec607d02ee9a69d0fd1c232fd91ba3e
serpro <none>
2022-08-12 17:38:28 +0300

    hw7. workspace, tcp test app socket

---
+++ socket/src/main.rs
@@ -0,0 +1,15 @@
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: TcpStream) {
+    println!("{:?}", stream);
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:81")?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
+}
