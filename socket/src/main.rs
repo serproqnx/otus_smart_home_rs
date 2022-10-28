af986e5de25752dc6e08ba8c4f1e316364815f9d
serpro <none>
2022-10-28 17:46:35 +0300

    hw12 gui 2

--- socket/src/main.rs
+++ socket/src/main.rs
@@ -13,14 +13,19 @@ use iced:: {  button, Button, Column, Element, Row, Sandbox, Settings, Text };
 
 #[derive(Debug, Clone)]
 enum Message {
-  Increment,
-  Decrement,
+  //Increment,
+  //Decrement,
+  TurnOn,
+  TurnOff,
 }
 
 struct Model {
-  counter: i64, 
-  button_inc: button::State,
-  button_dec: button::State,
+  //counter: i64, 
+  //button_inc: button::State,
+  //button_dec: button::State,
+  button_turn_on: button::State,
+  button_turn_off: button::State,
+  report: String,
 }
 
 impl Sandbox for Model {
@@ -28,9 +33,12 @@ impl Sandbox for Model {
 
   fn new() -> Self {
     Self {
-      counter: 42,
-      button_inc: Default::default(),
-      button_dec: Default::default(),
+      //counter: 42,
+      //button_inc: Default::default(),
+      //button_dec: Default::default(),
+      button_turn_on: Default::default(),
+      button_turn_off: Default::default(),
+      report: "Test_report".to_string(),
     }
   }
 
@@ -40,18 +48,34 @@ impl Sandbox for Model {
 
   fn update(&mut self, message: Self::Message) {
     match message {
-      Message::Increment => self.counter += 1,
-      Message::Decrement => self.counter -= 1,
+      //Message::Increment => self.counter += 1,
+      //Message::Decrement => self.counter -= 1,
+      Message::TurnOn => self.report = "Turned On".to_string(),
+      Message::TurnOff => self.report = "Turned Off".to_string(),
     }
   }
 
   fn view(&mut self) -> Element<'_, Self::Message> {
-    let text = Text::new(self.counter.to_string()).size(60);
+    //let text = Text::new(self.counter.to_string()).size(60);
       
-    let button_inc = Button::new(&mut self.button_inc, Text::new("Increment")).on_press(Message::Increment);
-    let button_dec = Button::new(&mut self.button_dec, Text::new("Decrement")).on_press(Message::Decrement);
+    //let button_inc = Button::new(&mut self.button_inc, Text::new("Increment")).on_press(Message::Increment);
+    //let button_dec = Button::new(&mut self.button_dec, Text::new("Decrement")).on_press(Message::Decrement);
+   
+    let report = Text::new(self.report.to_string()).size(20);
+    let button_turn_on = Button::new(&mut self.button_turn_on, Text::new("Turn on")).on_press(Message::TurnOn);
+    let button_turn_off = Button::new(&mut self.button_turn_off, Text::new("Turn off")).on_press(Message::TurnOff);
 
-    Column::new().padding(20).push(button_inc).push(text).push(button_dec).into()
+    Column::new()
+      .padding(20)
+      .push(report)
+      .push(button_turn_on)
+      .push(button_turn_off)
+      //.push(text)
+      //.push(button_inc)
+      //.push(button_dec)
+      .into()
+
+    
   } 
 
 }
@@ -118,45 +142,52 @@ async fn handle_client(mut stream: TcpStream, device: &mut Socket) {
 }
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-//async fn main() -> iced::Result {
+//async fn main() -> io::Result<()> {
+async fn main() -> iced::Result {
+  let t_net = tokio::spawn( async move { net().await });
 
-    let mut test_socket: Socket = Socket {
-      name: "Socket1",
-      about: "Real Socket 1",
-      on_status: false,
-      current_power_consumption: 42,
-      ip: SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8181),
-    };
-    
-    println!("SOCKET: {:?}", test_socket.ip);
-
-    let listener = TcpListener::bind(test_socket.ip).await?;
-
-  let t_gui = tokio::spawn( async move { gui().await });
-    //
-    // accept connections and process them serially for stream in listener. {
-    //    handle_client(stream?, &mut test_socket);
-    //}
-
-  t_gui.await?;
-
-    loop {
-      let (socket, _) = listener.accept().await?;
-      handle_client(socket, &mut test_socket).await;
-    }
-
-    //Ok(())
-}
-
-async fn gui() -> iced::Result {
   Model::run(
     Settings {
       window: iced::window::Settings {
-        size: (300, 200),
+        size: (300, 300),
         ..Default::default()
       },
       ..Default::default()
     }
-  )
+  ).unwrap();
+
+  t_net.await.unwrap().unwrap();
+
+  Ok(())
 }
+
+async fn net() -> io::Result<()> {
+  let mut test_socket: Socket = Socket {
+    name: "Socket1",
+    about: "Real Socket 1",
+    on_status: false,
+    current_power_consumption: 42,
+    ip: SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8181),
+  };
+  
+  println!("SOCKET: {:?}", test_socket.ip);
+
+  let listener = TcpListener::bind(test_socket.ip).await?;
+
+  loop {
+    let (socket, _) = listener.accept().await?;
+    handle_client(socket, &mut test_socket).await;
+  }
+}
+
+//async fn gui() -> iced::Result {
+  //Model::run(
+    //Settings {
+      //window: iced::window::Settings {
+        //size: (300, 200),
+        //..Default::default()
+      //},
+      //..Default::default()
+    //}
+  //)
+//}
